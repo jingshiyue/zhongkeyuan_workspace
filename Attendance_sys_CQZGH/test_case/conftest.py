f3c25087d11d1439fe144f6f769a447813137fc3
jingshiyue <173302591@qq.com>
2019-08-30 15:51:20 +0800

    完善conftest.py 文件里内容，更完善的pytest框架

--- Attendance_sys_CQZGH/test_case/conftest.py
+++ Attendance_sys_CQZGH/test_case/conftest.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 # Time : 2019/8/22 10:48 
 # Author : zcl
-import pytest
+import pytest,sys
+sys.path.append(r"D:\workfile\zhongkeyuan_workspace")
 from WuLanChaBuApi.common.mysql_class import *
+from Attendance_sys_CQZGH.utils.Log import mylog
+logger = mylog.get_log().get_logger()
 shujuku = DataBase("192.168.5.15", 3306, "root", "123456", "faceguard")
 staff_tuple = shujuku.find_all('SELECT * FROM face_image WHERE face_image.`name` LIKE "测试考勤%" ;')
 
 @pytest.fixture()
 def staff_dic():
+    logger.info("打开数据库连接！！！")
     staff = {}
     for i in staff_tuple:
         staff.setdefault(i[9],{})
         staff[i[9]].setdefault("num",i[0])
         staff[i[9]].setdefault("id",i[12])
-    return staff
+    yield staff
+    shujuku.close_database()
+    logger.info("断开数据库连接!!!")
--- Attendance_sys_CQZGH/test_case/test_考勤流程测试.py
+++ Attendance_sys_CQZGH/test_case/test_考勤流程测试.py
@@ -5,7 +5,6 @@ import pytest,sys,json
 from WuLanChaBuApi.TestApi.test_Match_Record.MatchRecord import MatchRecord
 from Attendance_sys_CQZGH.utils.common_method import *
 from Attendance_sys_CQZGH.Attendance_sys_CQZGH_api import *
-from Attendance_sys_CQZGH.utils.Log import mylog
 logger = mylog.get_log().get_logger()
 # from WuLanChaBuApi.TestApi.new_method import *
 from WuLanChaBuApi.TestApi.Test_regist.Regist import Regist
@@ -116,9 +115,9 @@ def test_create_data_process_04(staff_dic):
 ###############################
 
 # @pytest.mark.skip(reason="刷脸，同步到考勤系统里")
-def test_matchrecord_process_01(staff_dic,staff_num=10):
+def test_matchrecord_process_01(staff_dic,staff_num=5):
     """
-    产生刷脸数据
+    :param:刷脸的员工人数，最大不能超过注册的员工数量
     :return:
     """
     logger.info("**************%s 测试开始**************" % sys._getframe().f_code.co_name)
@@ -126,35 +125,38 @@ def test_matchrecord_process_01(staff_dic,staff_num=10):
     picpath = r"D:\workfile\zhongkeyuan_workspace\test_photoes\picture(现场照片)"
     records = []
     k = 100
-    for staff in staff_dic:
-        for time in ["20190828093000","20190828123000","20190828183000"]:  #一天的打卡时间
-        # for time in ["20190830120000"]:  # 一天的打卡时间
-            record = {
-                "id": get_uuid(),
-                "baseDeviceCode": "Device1",
-                "baseAreaCode": "T411",
-                "faceImageId": staff_dic[staff]["num"],
-                "personCode": staff_dic[staff]["id"],
-                "faceLibraryId": "4028858f6cd61b18016cd65d511c010b",
-                "areaName": "三级区域1",
-                "faceName": staff,
-                "codeType": "0",
-                "libraryName": "测试考勤",
-                "swipeTime":time, #20190826083000,20190826123000,20190826183000     yyyyMMddHHmmss
-                "livePhoto": to_base64(picpath + "\\" + "%d.jpg" % k),
-                "threshold": "0.8",
-                "matchingScore": "0.95555",
-                "passStatus": "1",  #1，表示成功
-                "personSex": "1",
-                "noPassReason": ""
-            }
-            records.append(record)
-        k += 1
+    m = 0
+    for staff, value in staff_dic.items():
+        if m < staff_num:
+            for time in ["20190821093000","20190821123000","20190821183000"]:  #一天的打卡时间
+            # for time in ["20190830120000"]:  # 一天的打卡时间
+                record = {
+                    "id": get_uuid(),
+                    "baseDeviceCode": "Device1",
+                    "baseAreaCode": "T411",
+                    "faceImageId": staff_dic[staff]["num"],
+                    "personCode": staff_dic[staff]["id"],
+                    "faceLibraryId": "4028858f6cd61b18016cd65d511c010b",
+                    "areaName": "三级区域1",
+                    "faceName": staff,
+                    "codeType": "0",
+                    "libraryName": "测试考勤",
+                    "swipeTime":time, #20190826083000,20190826123000,20190826183000     yyyyMMddHHmmss
+                    "livePhoto": to_base64(picpath + "\\" + "%d.jpg" % k),
+                    "threshold": "0.8",
+                    "matchingScore": "0.95555",
+                    "passStatus": "1",  #1，表示成功
+                    "personSex": "1",
+                    "noPassReason": ""
+                }
+                records.append(record)
+            k += 1
+            m += 1
     body = {
             "reqId": get_uuid(),
             "records": records,
              "num": len(records)
-        }
+            }
     res = _MatchRecord.api_face_matchrecord_sync(body)
     logger.info(res.text)
     assert json.loads(res.text)["status"] == 0
